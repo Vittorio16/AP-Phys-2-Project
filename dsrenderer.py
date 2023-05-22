54a616f042608f312dc11c3ed7bbb0cf01a88818
Joey R <joeyruan@zoho.com>
2023-05-21 20:30:09 -0700

    intensity change

--- dsrenderer.py
+++ dsrenderer.py
@@ -3,11 +3,16 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
-d = 5 # slit seperation
-max_y = 100 # wall distance
-max_x = 100 # peaks included
-point_step = 0.1 # amount of points between 0 and max
-wavelength = 5
+slit_sep = 154 # slit seperation in micrometers
+d = slit_sep * 10**(-3) # slit sep in mm
+max_y = 1400 # wall distance in mm
+max_x = 10000 # peaks included 
+point_step = 5 # resolution, should be integer
+wavelength_input = 632.8 # wavelength in nanometers
+λ = wavelength_input * 10**(-6) # wavelength in millimeters
+f = 3.00 * (10**8) / (wavelength_input * 10**(-9))
+h = 4.135 * 10**(-15)
+π = np.pi
 
 x_positive = d/2
 x_negative = 0 - x_positive
@@ -22,8 +27,8 @@ for y in np.arange(0, max_y, point_step):
     for x in np.arange(-max_x, max_x, point_step):
         length_positive = np.sqrt((y**2) + ((x - x_positive)**2))
         length_negative = np.sqrt((y**2) + ((x - x_negative)**2))
-        z_positive = np.sin(((2 * np.pi) / wavelength) * length_positive)
-        z_negative = np.sin(((2 * np.pi) / wavelength) * length_negative)
+        z_positive = np.sin(2 * π / λ * length_positive) * (h * f / 4 * π * length_positive**2)
+        z_negative = np.sin(2 * π / λ * length_negative) * (h * f / 4 * π * length_negative**2)
         z = z_positive + z_negative
         x_list.append(x)
         y_list.append(y)
@@ -39,4 +44,4 @@ ax = plt.axes(projection='3d')
 
 surf = ax.plot_trisurf(X, Y, Z, cmap = plt.cm.cividis)
 
-plt.savefig('foo.png', bbox_inches='tight')
+plt.savefig('sin.png', bbox_inches='tight')
