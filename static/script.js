8962f47e5b587eea4f71826a9006843f7b71d0f9
Vittorio <vittorio.latrofa@gmail.com>
2023-06-15 08:18:57 -0700

    Fixing graph display

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", setTimeout);
-
+window.addEventListener("load", displayText);
 // Hide error messages after a while
 setTimeout(() => {
   const box = document.getElementById('error-message');
@@ -9,3 +9,13 @@ setTimeout(() => {
   // 👇️ hides element (still takes up space on page)
   // box.style.visibility = 'hidden';
 }, 6000); // 👈️ time in milliseconds
+
+function displayText() {
+  isImage = document.getElementById("graph");
+  if (isImage == "True") {
+    document.getElementById("input-form").style.display = "none"
+  }
+  else {
+    document.getElementById("input-form").style.display = "inline-block"
+  }
+}
