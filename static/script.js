f2a3f43a3ea42942dd7735afaa475c7f7d48c417
davin00 <davin.aoyama@misd400.org>
2023-06-16 08:48:35 -0700

    input bugs and imporvements

--- static/script.js
+++ static/script.js
@@ -2,17 +2,13 @@ window.addEventListener("load", displayText);
 
 function displayText() {
   let isImage = document.getElementById("graph");
-  console.log(isImage.value);
   if (isImage.value == "True") {
-    console.log("hiding");
     document.getElementById("input-form").style.display = "none";
     document.getElementById("info-paragraph").style.display = "none";
 
   }
   else {
-    console.log("not hiding");
     document.getElementById("input-form").style.display = "inline-block";
     document.getElementById("info-paragraph").style.display = "block";
-
   }
 }
